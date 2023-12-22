e7ffd9823afcc195bd624de5486b015a23196cd7
Andrei Tuicu <tuicu@adobe.com>
2023-12-22 15:29:22 +0200

    feat - Lighthouse Viewer in the customer's domain

--- psi/boostrap.js
+++ psi/boostrap.js
@@ -35,6 +35,7 @@ async function loadLighouseViewer() {
   }
 
   iframe.src = viewerUrl;
+  iframe.style.cssText = "overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px";
   document.querySelector('body').appendChild(iframe);
 }
 
