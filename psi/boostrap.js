8ad6da69db835f36291178a05477e931193e7f62
Andrei Tuicu <tuicu@adobe.com>
2024-02-14 16:40:12 +0100

    feat - Lighthouse Viewer in the customer's domain

--- psi/boostrap.js
+++ psi/boostrap.js
@@ -39,9 +39,10 @@ async function loadLighouseViewer() {
   await loadCSS(new URL('/psi/boostrap.css', origin).toString());
 
   const iframe = document.createElement('iframe');
+  // this would be https://psi.experiencecloud.live
   const iframeOrigin = 'https://local-viewer--eaas-lh-test--andreituicu.hlx.page';
+  // this would be https://psi.experiencecloud.live/viewer/
   iframe.src = `${iframeOrigin}/lighthouse-viewer/index.html`;
-  // iframe.style.cssText = "overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px";
   document.querySelector('body').appendChild(iframe);
 
   const searchParams = new URLSearchParams(window.location.search);
