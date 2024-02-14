835e3543a112e90cec7b42ac198b5c3595a46ba9
Andrei Tuicu <tuicu@adobe.com>
2024-02-14 15:16:25 +0100

    feat - Lighthouse Viewer in the customer's domain

--- lighthouse-viewer/src/iframe-message-handler.js
+++ lighthouse-viewer/src/iframe-message-handler.js
@@ -6,8 +6,7 @@ window.addEventListener(
     console.log(window.viewer);
     if (!(event.origin.endsWith('hlx.page')|| event.origin.endsWith('aem.page'))) return;
     if (event.data.type === 'lighthouse-report') {
-      let psiReportBlob = new Blob([JSON.stringify(event.data.report)], {type: "application/json"});
-
+      window.viewer._replaceReportHtml(event.data.report);
     }
   },
   false,
