de0d7569ca56288b15ed6143bcbd38c796d1e904
Andrei Tuicu <tuicu@adobe.com>
2024-02-16 10:55:41 +0100

    feat - Lighthouse Viewer in the customer's domain

--- lighthouse-viewer/src/iframe-message-handler.js
+++ lighthouse-viewer/src/iframe-message-handler.js
@@ -8,3 +8,9 @@ window.addEventListener(
   },
   false,
 );
+
+// we are in an iframe - let the parent know we are ready
+if (window !== window.parent) {
+  console.log('sending ready message');
+  window.parent.postMessage({rendered: true}, '*');
+}
